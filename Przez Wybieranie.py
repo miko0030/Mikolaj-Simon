3b18d7a1d4c79f39482bfcee6b64b8afa103215a
miko0030 <62652973+miko0030@users.noreply.github.com>
2020-03-26 12:14:33 +0100

    Add files via upload

---
+++ Przez Wybieranie.py
@@ -0,0 +1,20 @@
+#sortowanie przez wybieranie
+import datetime
+import random
+
+lista = [int(x) for x in input().split()]
+def sortowanie_przez_wybieranie(lista):
+    for i in range(len(lista)):
+        numer = i
+        for j in range(i+1,len(lista)):
+            if lista[numer]> lista[j]:
+                numer = j
+                lista[i],lista[numer]= lista[numer], lista[i]
+    return lista
+
+czas1 = datetime.datetime.utcnow().timestamp()
+sortowanie_przez_wybieranie(lista)
+czas2 = datetime.datetime.utcnow().timestamp()
+print(sortowanie_przez_wybieranie(lista))
+print((czas2-czas1), 'seconds')
+
